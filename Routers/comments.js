d2f78ce49c7e8ec5031e73066609583e318e5f6f
MarwahHatamleh <marwahatamleh98@gmail.com>
2025-05-22 22:04:51 +0300

    added getCommentsByArticle

--- Routers/comments.js
+++ Routers/comments.js
@@ -4,11 +4,12 @@ const authMiddlewares = require("../middlewares/authentication");
 const authzMiddlewares = require("../middlewares/authorization");
 
 
-const { createNewComment } = require("../contollers/comments");
+const { createNewComment , getCommentsByArticle} = require("../contollers/comments");
 
 const commentRouter = express.Router();
 
 commentRouter.post("/:id", authMiddlewares,createNewComment);
+commentRouter.get("/:id", authMiddlewares,getCommentsByArticle);
 
 module.exports = commentRouter;
 /*
--- contollers/comments.js
+++ contollers/comments.js
@@ -29,7 +29,30 @@ const createNewComment = (req, res) => {
     });
 };
 // This function returns the comments
-const getCommentsByArticle = (req, res) => {};
+const getCommentsByArticle = (req, res) => {
+  const { id } = req.params;
+
+  pool
+    .query(
+      `SELECT users.id , users.firstname , comments.comment , comments.comment , comments.article_id , comments.commenter_id FROM comments FULL OUTER JOIN  users ON users.id = comments.commenter_id  WHERE commenter_id = $1`,
+      [id]
+    )
+    .then((results) => {
+      res.status(200).json({
+        success: true,
+        massage: `All comments for article: ${id}`,
+        comments: results.rows,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        massage: "Server Error",
+        err: err.message,
+      });
+    });
+};
+
 module.exports = {
   createNewComment,
   getCommentsByArticle,
